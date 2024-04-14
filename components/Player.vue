62b771ccf6cdde59ccc51c8c097ea578c1bf30e2
vanminhquangtri <vanminhquangtri@gmail.com>
2024-04-14 15:34:01 +0700

    fix auto play mobile

--- components/Player.vue
+++ components/Player.vue
@@ -1,10 +1,23 @@
 <template>
   <div class="video-container">
-    <video id="video" width="640" controls autoplay playsinline></video>
+    <video
+      ref="video"
+      id="video"
+      width="640"
+      playsinline
+      controls
+      @loadedmetadata="playVideo"
+    ></video>
   </div>
+
+  <div></div>
+
+  <div v-html="error" />
 </template>
 
 <script setup lang="ts">
+const video = ref<HTMLVideoElement | null>(null);
+const error = ref('');
 const manifestUri =
   'https://vod06-cdn.fptplay.net/POVOD/encoded/2024/01/28/theaccidentalspy-2001-hk-1706408052/master.m3u8';
 
@@ -42,13 +55,57 @@ async function initPlayer() {
   // This is an asynchronous process.
   try {
     await player.load(manifestUri);
+    // listenPlayer();
     // This runs if the asynchronous load is successful.
     console.log('The video has now been loaded!');
+    // playVideo();
   } catch (e) {
     // onError is executed if the asynchronous load fails.
     onError(e);
   }
 }
+const listenPlayer = () => {
+  /*@ts-ignore*/
+  const eventManager = new window.shaka.util.EventManager();
+  eventManager.listenOnce(video.value, `play`, (ev: any) => {
+    playVideo(ev);
+  });
+  eventManager.listenOnce(video.value, `playing`, () => {
+    if (video.value) {
+      // video.value.volume = 1;
+    }
+  });
+};
+
+const playVideo = (ev: any) => {
+  error.value = error.value + '<br/>' + ' playVideo ' + ev.type;
+  if (!video.value) {
+    return;
+  }
+
+  try {
+    const promise = video.value.play();
+    if (promise) {
+      promise
+        .then(() => {
+          console.log('play success');
+        })
+        .catch((e) => {
+          console.log('ERROR auto play', e);
+          error.value = e.message;
+          if (video.value) {
+            video.value.muted = true;
+            playVideo(ev);
+          }
+        });
+    } else {
+      error.value = 'no promise';
+    }
+  } catch (e: any) {
+    error.value = e.message;
+  }
+};
+
 /*@ts-ignore*/
 function onErrorEvent(event) {
   // Extract the shaka.util.Error object from the event.
